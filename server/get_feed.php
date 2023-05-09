ad76386e59fd942ec509d274958d9aedf9c91451
Max Chu <maxchu2021@gmail.com>
2023-05-09 11:50:05 +0800

    Update get_feed.php

--- server/get_feed.php
+++ server/get_feed.php
@@ -1,11 +1,27 @@
 <?php
 
 function get_feed($feed_url) {
-    $content = file_get_contents($feed_url);
-    $content = str_replace("<content:encoded>","<contentEncoded>",$content);
-    $content = str_replace("</content:encoded>","</contentEncoded>",$content);
-    $x = new SimpleXmlElement($content);
-    return $x->channel->item;
+    $context = stream_context_create(
+        array(
+            "http" => array(
+                "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+            )
+        )
+    );
+    $filename = __DIR__ . '/' . str_replace('/', '-', $feed_url) . '-' . date('Y-m-d') . '.txt';
+    if (file_exists($filename)) {
+        $myfile = fopen($filename, "r");
+        $content = fread($myfile,filesize($filename));
+    } else {
+        $content = file_get_contents($feed_url, false, $context);
+        $myfile = fopen($filename, "w");
+        fwrite($myfile, $content);
+    }
+    fclose($myfile);
+    if ($content) {
+        $x = new SimpleXmlElement($content);
+        return $x->channel->item;
+    }
 }
 
 function get_imgs($file) {
@@ -54,23 +70,29 @@ function special_character_decode($str) {
 }
 
 $feed = get_feed($_GET['url']);
+if (!$feed) {
+    return;
+}
 $data = array();
 
 foreach($feed as $entry){
-    preg_match('/< *img[^>]*src *= *["\']?([^"\']*)/i', $entry->description, $matches);
-	$cover = $matches[1];
-
-	if (empty($cover)) {
-	    preg_match('/< *img[^>]*src *= *["\']?([^"\']*)/i', $entry->contentEncoded, $matches);
-		$cover = $matches[1];
-	}
+    $cover = '';
+    if ($entry->children( 'media', True )->content) {
+        $cover = $entry->children( 'media', True )->content->attributes()['url'];
+    } else if ($entry->children( 'media', True )->thumbnail) {
+        $cover = $entry->children( 'media', True )->thumbnail->attributes()['url'];
+    } else {
+        preg_match('/< *img[^>]*src *= *["\']?([^"\']*)/i', $entry->description, $matches);
+        if (isset($matches[1])) {
+            $cover = $matches[1];
+        }
+    }
 
     $data[] = array(
         'link' => strip_tags($entry->link),
         'title' => strip_tags($entry->title),
-        'image' => strip_tags($entry->image->url),
         'description' => strip_tags($entry->description),
-        'cover' => $cover
+        'cover' => (string) $cover
     );
 }
 
